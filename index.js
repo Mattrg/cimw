e69d208d6185d6996b756eae699a42cf9b8bb366
Mattrg <Mattrg@users.noreply.github.com>
2019-12-05 05:31:33 -0800

    test

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 var express = require('express')
 var app = express()
 var http = require('http').createServer(app);
-var io = require('socket.io')(http);
+//var io = require('socket.io')(http);
 const router = express.Router();
-const path = __dirname + '/'
+//const path = __dirname + '/'
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+//app.set('port', (process.env.PORT || 5000))
+//app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
   response.send('Hello World!')
@@ -20,18 +20,25 @@ app.get("/index", function(req,res){
   res.sendFile(path + "index.html");
 })
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-    console.log('message: ' + msg);
-  });
-  console.log('a user connected');
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
-  });
+const socketIO = require('socket.io');
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+const INDEX = path.join(__dirname, 'index.html');
+
+const server = express()
+  .use((req, res) => res.sendFile(INDEX) )
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+const io = socketIO(server);
+
+io.on('connection', (socket) => {
+  console.log('Client connected');
+  socket.on('disconnect', () => console.log('Client disconnected'));
 });
 
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+
+//app.listen(app.get('port'), function() {
+  //console.log("Node app is running at localhost:" + app.get('port'))
+//})
